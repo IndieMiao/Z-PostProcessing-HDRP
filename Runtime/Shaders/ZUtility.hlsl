db23246f19393450203a423e52eddf3dbd0e03d3
IndieMiao <IndieMiao@126.com>
2021-07-28 18:14:20 +0800

    update Zutility

--- Runtime/Shaders/ZUtility.hlsl
+++ Runtime/Shaders/ZUtility.hlsl
@@ -6,4 +6,10 @@ float SoftThreshHold(float x, float threshold, float size)
 return saturate((x-threshold) / max(size,Eps_float()));
 }
 
+float remap(float inputValue, float oldmin, float oldmax, float outmin, float outmax )
+{
+	float clampValue = clamp(inputValue,oldmin, oldmax);
+	return (outmax - outmin)*(clampValue - oldmin) + outmin;
+}
+
 #endif //  Z_UTILITY_LIBRARY
